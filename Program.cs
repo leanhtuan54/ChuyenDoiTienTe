9fe5fd5da76348510faf7e7db1d56e0fa5f9ddc5
leanhtuan54 <anhtuanle0054@gmail.com>
2024-08-06 18:28:20 +0700

    fisrt commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UngDungChuyenDoiTienTe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float usdCurrency;
+            float vndCurrency;
+
+            Console.Write("Enter USD currency: ");
+            usdCurrency = float.Parse(Console.ReadLine());
+
+            vndCurrency = usdCurrency*23000;
+
+            Console.Write($"Exchange to VND currency: {vndCurrency:F2} VND");
+        }
+    }
+}
